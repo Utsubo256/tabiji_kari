d1133bf5733b7d68bf574da29cc16532f21d26c7
Utsubo256 <greeeentea256@gmail.com>
2022-10-14 14:57:32 +0900

    ログインAPIの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(email: params[:email])&.authenticate(params[:password])
+
+    payload = {
+      iss: "Tabiji",
+      sub: user.id,
+      exp: (DateTime.current + 14.days).to_i
+    }
+
+    rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
+
+    token = JWT.encode(payload, rsa_private, "RS256")
+
+    cookies[:token] = token
+
+    render status: :created
   end
 end
